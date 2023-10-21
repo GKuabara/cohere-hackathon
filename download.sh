d14e7d36c913502f14d6fdc1bd94e2d7926477ee
Cesar <cesarguibo@gmail.com>
2023-10-21 17:46:01 -0300

    Added musics dataset download script

---
+++ download.sh
@@ -0,0 +1,4 @@
+curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+echo "f0df49ffe5f2a6008d7dc83c6915b31835dfe733  fma_metadata.zip" | sha1sum -c -
+curl -O https://os.unil.cloud.switch.ch/fma/fma_small.zip
+echo "ade154f733639d52e35e32f5593efe5be76c6d70  fma_small.zip"    | sha1sum -c -
